88e4d24539c1ff132f9704e92a61897194342768
yonghwan1998 <ss9504ss@naver.com>
2023-06-06 16:39:08 +0900

    기본 문법 학습

---
+++ 01_basic/practice.py
@@ -0,0 +1,39 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5 + 3)
+print(2 * 3)
+print(3 * (3 + 1))
+
+
+print("================================================")
+
+print("python")
+print("풍선")
+print("lol " * 5)
+
+print("================================================")
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(not True)
+print(not False)
+print(not (5 < 10))
+
+print("================================================")
+
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+hobby = "공놀이"
+print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
+print(name + "는 어른일까요? " + str(is_adult))
+
+print("================================================")
