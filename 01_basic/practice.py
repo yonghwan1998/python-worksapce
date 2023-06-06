925e3a53853564db14dd3bb1797d1d621c64e9ba
yonghwan1998 <ss9504ss@naver.com>
2023-06-06 17:10:05 +0900

    기초 문법 학습

--- 01_basic/practice.py
+++ 01_basic/practice.py
@@ -32,8 +32,62 @@ is_adult = age >= 3
 
 print("우리집 " + animal + "의 이름은 " + name + "에요")
 hobby = "공놀이"
-print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+print(name + "는 " + str(age) + "살이며, " + hobby + "을(를) 아주 좋아해요")
 print(name, "는 ", age, "살이며, ", hobby, "을 아주 좋아해요")
 print(name + "는 어른일까요? " + str(is_adult))
+
+print("================================================")
+# Quiz.1
+
+station = "사당"
+print(station + " 행 열차가 들어오고 있습니다.")
+
+station = "신도림"
+print(station + " 행 열차가 들어오고 있습니다.")
+
+station = "인천공항"
+print(station + " 행 열차가 들어오고 있습니다.")
+
+
+print("================================================")
+
+print(2**3)
+print(5 % 3)
+print(50 / 3)
+print(50 // 3)
+
+print(5 >= 3)
+print(5 < 3)
+print(3 == 3)
+
+print(3 + 4 == 7)
+print(1 != 32)
+print(1 != 1)
+
+print((3 > 0) and (3 < 5))
+print((3 > 0) & (3 < 5))
+
+print((3 > 0) or (3 > 5))
+print((3 > 0) | (3 > 5))
+
+print((5 > 4 > 3))
+print((5 > 4 > 7))
+
+print("================================================")
+
+number = 2 + 3 * 4
+print(number)
+
+number = number + 2
+print(number)
+
+number += 2
+print(number)
+
+number *= 2
+print(number)
+
+number %= 2
+print(number)
 
 print("================================================")
