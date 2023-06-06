cfddf9607923ae2875c0959c92da356c959e0de4
yonghwan1998 <ss9504ss@naver.com>
2023-06-06 17:23:14 +0900

    math 학습

---
+++ 01_basic/math.py
@@ -0,0 +1,57 @@
+print(abs(-5))
+print(pow(4, 2))
+print(max(5, 12))
+print(min(5, 12))
+print(round(3.14))
+print(round(4.59))
+
+print("================================================")
+
+from math import *
+
+print(floor(4.99))
+print(ceil(3.14))
+print(sqrt(16))
+
+print("================================================")
+
+from random import *
+
+print(random())
+print(random() * 10)
+print(int(random() * 10))
+print(int(random() * 10))
+print(int(random() * 10))
+
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+
+print("================================================")
+
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+print(int(random() * 10) + 1)
+
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+print(randrange(1, 46))
+
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+print(randint(1, 45))
+
+print("================================================")
+
+# Quiz.2
+day = randint(4, 28)
+print("오프라인 스터디 모임 날짜는 매월 " + str(day) + "일로 선정되었습니다.")
