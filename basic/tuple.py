9d87457667b51c6dfae666b2cba1b1d737dca284
yonghwan1998 <ss9504ss@naver.com>
2023-06-07 09:59:39 +0900

    tuple 학습

---
+++ basic/tuple.py
@@ -0,0 +1,10 @@
+menu = "돈까스", "치즈까스"
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스")
+
+print("================================================")
+
+(name, age, hobby) = ("tuple", 20, "coding")
+print(name, age, hobby)
