baf361b68f41326bffcb6409a04dc3549a9c44cc
yonghwan1998 <ss9504ss@naver.com>
2023-06-22 10:02:19 +0900

    quiz

--- basic/quiz.py
+++ basic/quiz.py
@@ -115,3 +115,40 @@
 #         report_file.write(f"\n부서 : ")
 #         report_file.write(f"\n이름 : ")
 #         report_file.write(f"\n업무 요약 : ")
+
+# print("================================================")
+
+# Quiz.7
+
+
+class House:
+    def __init__(self, location, house_type, deal_type, price, completion_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.completion_year = completion_year
+
+    def show_detail(self):
+        print(
+            self.location,
+            self.house_type,
+            self.deal_type,
+            self.price,
+            self.completion_year,
+        )
+
+
+house1 = House("강남", "아파트", "매매", "10억", "2010년")
+house2 = House("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House("송파", "빌라", "월세", "500/50", "2000년")
+
+houses = []
+houses.append(house1)
+houses.append(house2)
+houses.append(house3)
+
+print(f"총 {len(houses)}대의 매물이 있습니다.")
+
+for house in houses:
+    house.show_detail()
