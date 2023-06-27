2693c62c7d2aa3f2c5cbd3c834f6aa7f575edada
yonghwan1998 <ss9504ss@naver.com>
2023-06-27 18:00:54 +0900

    inner function 학습

---
+++ basic/inner_functon.py
@@ -0,0 +1,24 @@
+# input
+
+# language = input("무슨 언어를 좋아하세요?")
+# print(f"{language}은 아주 좋은 언어입니다!")
+
+# print("================================================")
+
+# dir
+
+# print(dir())
+# import random
+
+# print(dir())
+# import pickle
+
+# print(dir())
+
+# print(dir(random))
+
+# list = [1, 2, 3]
+# print(dir(list))
+
+# name = "Jim"
+# print(dir(name))
