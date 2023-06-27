9f886884b92626a3041e911df22b244cffad55c0
yonghwan1998 <ss9504ss@naver.com>
2023-06-27 17:25:47 +0900

    module 학습

---
+++ basic/module.py
@@ -0,0 +1,33 @@
+# import theater_module
+
+# theater_module.price(3)
+# theater_module.price_morning(4)
+# theater_module.price_soldier(5)
+
+# print("================================================")
+# import theater_module as mv
+
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
+
+# print("================================================")
+
+# from theater_module import *
+
+# price(3)
+# price_morning(4)
+# price_soldier(5)
+
+# print("================================================")
+
+# from theater_module import price, price_morning
+
+# price(3)
+# price_morning(4)
+
+# print("================================================")
+
+from theater_module import price_soldier as price
+
+price(5)
---
+++ basic/theater_module.py
@@ -0,0 +1,13 @@
+# 일반 가격
+def price(people):
+    print(f"{people}명 가격은 {people*10000}원 입니다.")
+
+
+# 조조 할인 가격
+def price_morning(people):
+    print(f"{people}명 조조 할인 가격은 {people*6000}원 입니다.")
+
+
+# 군인 할인 가격
+def price_soldier(people):
+    print(f"{people}명 조조 할인 가격은 {people*4000}원 입니다.")
